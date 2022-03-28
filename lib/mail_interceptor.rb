aa516261eab15565f514071261c5a3d32fc7e292
Ghouse Mohamed <ghousemohamed7126@gmail.com>
2022-03-28 13:24:55 +0530

    Fixes activesupport for 7.0

--- lib/mail_interceptor.rb
+++ lib/mail_interceptor.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "active_support"
 require 'active_support/core_ext/object/blank'
 require 'active_support/core_ext/array'
 require 'mail_interceptor/version'
